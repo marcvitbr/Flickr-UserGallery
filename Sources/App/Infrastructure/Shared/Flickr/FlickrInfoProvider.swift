9f1cc2e198bcd08232a8d7e28abb28aa178e2528
Marcelo Vitoria <marcvit@gmail.com>
2019-07-21 17:47:25 -0300

    Create class to provide info about Flickr's endpoints

---
+++ Sources/App/Infrastructure/Shared/Flickr/FlickrInfoProvider.swift
@@ -0,0 +1,41 @@
+//
+//  FlickrInfoProvider.swift
+//  Flickr-UserGallery
+//
+//  Created by Marcelo Vitoria on 21/07/19.
+//  Copyright © 2019 Marcelo Vitoria. All rights reserved.
+//
+
+import Foundation
+
+final class FlickrInfoProvider {
+    private static let apiKey = "f9cc014fa76b098f9e82f1c288379ea1"
+
+    static let apiURL = URL(string: "https://api.flickr.com/services/rest")!
+
+    static func parametersForGetPublicPhotos(userId: String, page: Int) -> [String: String] {
+        return ["method": "flickr.people.getPublicPhotos",
+                "api_key": FlickrInfoProvider.apiKey,
+                "user_id": userId,
+                "format": "json",
+                "nojsoncallback": "1",
+                "page": String(page),
+                "per_page": "50"]
+    }
+
+    static func parametersForGetSizesMethod(photoId: String) -> [String: String] {
+        return ["method": "flickr.photos.getSizes",
+                "api_key": FlickrInfoProvider.apiKey,
+                "photo_id": photoId,
+                "format": "json",
+                "nojsoncallback": "1"]
+    }
+
+    static func parametersForGetInfoMethod(photoId: String) -> [String: String] {
+        return ["method": "flickr.photos.getInfo",
+                "api_key": FlickrInfoProvider.apiKey,
+                "photo_id": photoId,
+                "format": "json",
+                "nojsoncallback": "1"]
+    }
+}
